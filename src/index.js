64eccefd0b2632dc9bdbe8c07a074e21fe8bda1e
Carlos Brown <cbrownc97@gmail.com>
2020-11-16 13:31:40 -0300

    add public folder

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(routes)
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.listen(3000, () => {
     console.log('listen on port 3000')
